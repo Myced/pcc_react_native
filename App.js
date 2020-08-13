ad4cd843c8e82efcd400682c743e73704bf06638
Ndi Cedric Tifuh <tncedric@gmail.com>
2020-08-13 14:56:01 +0100

    Added asset folder and sqlite database - Installed library for Picker - Started work on Diary Screen

--- App.js
+++ App.js
@@ -2,9 +2,26 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider as PaperProvider, Badge } from 'react-native-paper';
+import SQLite from 'react-native-sqlite-storage';
 import BottomNav from './src/components/BottomNav';
 import MyRouter from './src/Router';
 
+function checkDatabase(){
+    global.db = SQLite.openDatabase(
+        {
+            name: 'SQLite',
+            location: 'default',
+            createFromLocation: '~pcc.db',
+        },
+
+        () => { },
+
+        error => {
+             console.log("ERROR: " + error);
+        }
+
+    );
+}
 
 export default function App() {
   
--- src/Screens/DiaryScreen.js
+++ src/Screens/DiaryScreen.js
@@ -1,11 +1,58 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
+import { Picker } from '@react-native-community/picker';
+
+import { colors } from '../config/Config';
 
 class DiaryScreen extends Component {
+    state = {
+        day: '',
+        month: ''
+    };
+
     render() {
         return (
-            <Text> Diary </Text>
+            <View style={styles.containerStyle}>
+                <View>
+                    <Text style={styles.titleTextStyle}>
+                        CHURCH DIARY
+                    </Text>
+                </View>
+
+                <View>
+                    <View>
+                        <Text>
+                            Day
+                        </Text>
+                        
+                        <Picker
+                            selectedValue={this.state.language}
+                            style={{height: 50, width: 100}}
+                            onValueChange={(itemValue, itemIndex) =>
+                                this.setState({language: itemValue})
+                            }>
+                            <Picker.Item label="Java" value="java" />
+                            <Picker.Item label="JavaScript" value="js" />
+                        </Picker>
+                    </View>
+                </View>
+            </View>
         )
+    }
+}
+
+const days = ["01", "02", "03", "04", "05", "06", "07", "08", "09"];
+const months = ["January", "February", "March", "April", "May", "June"];
+
+const styles = {
+    containerStyle: {
+        padding: 5
+    },
+
+    titleTextStyle: {
+        color: colors.primary,
+        fontSize: 25,
+        fontWeight: 'bold',
     }
 }
 
