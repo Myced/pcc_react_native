a55eb692e28dd34837ddbe9957f173551ed1bfc7
Ndi Cedric Tifuh <tncedric@gmail.com>
2021-10-04 12:14:38 +0100

    Added payment for HYmn screen

--- src/Screens/HymnScreen.js
+++ src/Screens/HymnScreen.js
@@ -2,10 +2,94 @@ import React, { Component } from 'react';
 import { Text, View, FlatList, TouchableHighlight } from 'react-native';
 import { Paragraph, Title } from 'react-native-paper';
 import { Actions } from 'react-native-router-flux';
+import AsyncStorage from '@react-native-community/async-storage';
+import { Button, Icon } from 'react-native-elements';
+import { Avatar, Card, Divider, DefaultTheme } from 'react-native-paper';
 
 import Hymns from '../dataTest/HymnsData.json';
+import AsyncKeys from '../utils/AsyncKeys';
+import { ItemTypes, getItemCost } from '../utils/ItemType';
+import { executeSQLQuery } from '../utils/SQLUtil';
 
 class HymnScreen extends Component {
+
+    state = {
+        isPurchased: false,
+        itemCode: '',
+        itemType: '',
+        itemTitle: "Church Hymn Book",
+        year: null,
+        user: null,
+    }
+
+    UNSAFE_componentWillMount()
+    {
+        this.setUser();
+        this.checkHymnAvailability();
+
+        const date = new Date();
+        const year = String(date.getFullYear());
+        const itemType = ItemTypes.HYMN;
+        const itemCode = ItemTypes.HYMN;
+
+        this.setState({year, itemCode, itemType})
+    }
+
+    setUser()
+    {
+        //get the currently logged in user 
+        const UserPromise = AsyncStorage.getItem(AsyncKeys.userKey);
+
+        UserPromise.then( user => {
+            const finalUser = JSON.parse(user);
+            this.setState({user: finalUser});
+        } )
+        .catch ( error => console.error(error) );
+    }
+
+    checkHymnAvailability()
+    {
+        
+        const query = "SELECT * FROM `purchases` WHERE `item_type` = '" + ItemTypes.HYMN + "'";
+        const params = [];
+
+        executeSQLQuery(query, params)
+            .then( rows => {
+
+                let isPurchased = false;
+
+                rows.forEach(item => {
+                    isPurchased = true;
+                })
+
+                this.setState({isPurchased});
+            } )
+            .catch(error => {
+                console.error(error);
+                
+            })
+    }
+
+    buyHymn()
+    {
+		const year = this.state.year;
+        const user = this.state.user;
+        const amount = getItemCost(ItemTypes.HYMN);
+        const title = this.state.itemTitle;
+        const itemType = this.state.itemType;
+        const itemCode = this.state.itemCode;
+
+        const data = {
+            year,
+            user,
+            amount,
+            title,
+            itemType,
+            itemCode
+        };
+
+        Actions.paymentMethod(data);
+    }
 
     openHymn(hymn)
     {
@@ -37,15 +121,53 @@ class HymnScreen extends Component {
         );
     }
     render() {
-        return (
-            <View>
-                <FlatList
-                    keyExtractor={item => "h" + item.number}
-                    data={Hymns}
-                    renderItem={this.renderItem.bind(this)}
-                />
-            </View>
-        )
+        const LeftContent = props => <Avatar.Icon icon="alert" {...props} theme={theme}  />
+
+        if( this.state.isPurchased )
+        {
+            return (
+                <View>
+                    <FlatList
+                        keyExtractor={item => "h" + item.number}
+                        data={Hymns}
+                        renderItem={this.renderItem.bind(this)}
+                    />
+                </View>
+            )
+        }
+        else {
+            return (
+                <View style={ styles.centerView }>
+                    <View style={styles.errorView}>
+                        <Card elevation={2}>
+                            <Card.Title title="Error " subtitle="" left={LeftContent} />
+                            <Divider />
+                            <Card.Content style={{paddingTop: 10, paddingBottom: 10}}>
+                                <View>
+                                    <Text style={styles.errorText}>
+                                        You have not yet bought the CHURCH HYMN BOOK.
+                                    </Text>
+                                    <Text style={styles.errorText}>
+                                        Click on the button below to purchase it.
+                                    </Text>
+                                    <Text style={styles.errorPriceText}>
+                                        Price: { getItemCost(ItemTypes.HYMN) } FCFA
+                                    </Text>
+                                </View>
+                            </Card.Content>
+                            <Divider />
+                            <Card.Actions style={{justifyContent: 'flex-end'}}>
+                                <Button
+                                    title="Buy"
+                                    buttonStyle={[styles.buttonStyle, styles.buyButtonStyle]}
+                                    riased
+                                    onPress={this.buyHymn.bind(this)} />
+                            </Card.Actions>
+                        </Card>
+                    </View>
+                </View>
+            )
+        }
     }
 }
 
@@ -76,7 +198,52 @@ const styles = {
     hymnTitleContainerStyle: {
         justifyContent: 'space-around',
         paddingLeft: 10,
+    },
+
+    buttonStyle: {
+        backgroundColor: 'purple'
+    },
+
+    centerView: {
+        flex: 1,
+        justifyContent: 'center',
+        // alignItems: 'center'
+    },
+
+    errorView: {
+        justifyContent: 'center',
+        marginTop: 20,
+        marginBottom: 20,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+
+    errorText: {
+        fontSize: 16,
+        color: 'red',
+        fontWeight: 'bold',
+    },
+
+    errorPriceText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10
+    },
+
+    buyButtonStyle: {
+        paddingLeft: 50,
+        paddingRight: 50
     }
 }
+
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        primary: 'red',
+        accent: '#f1c40f',
+    },
+};
 
 export default HymnScreen;
